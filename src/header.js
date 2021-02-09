26e37a2645e035407f4ebadc7daa8a7912adabe5
KB <katrinkajb@gmail.com>
2021-02-08 17:19:22 -0800

    icons and styling

--- src/header.js
+++ src/header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import ButtonList from './button-list.js';
 
 export default class Header extends React.Component {
     render() {
@@ -6,9 +7,20 @@ export default class Header extends React.Component {
         <div className="App">
             <header className="App-header">
                 <img alt='logo' src="logo.png" />
-                <div>title</div>
+                <div className='title-text'>Delicious</div>
                 <div></div>
-                <div>Icons</div>
+                <div>
+                    <span className="buttonList">
+                        <img alt='Facebook icon' src='./fb-icon.png'/>
+                        <img alt='Twitter icon' src='./twit-icon.png'/>
+                        <img alt='G+ icon' src='./gp-icon.png'/>
+                        <img alt='Instagram icon' src='./insta-icon.png'/>
+                        <img alt='Flic icon' src='./flic-icon.png'/>
+                        <img alt='Pinterest icon' src='./pint-icon.png'/>
+                        <img alt='RSS icon' src='./rss-icon.png'/>
+                        <img alt='Email icon' src='./mail-icon.png'/>
+                    </span>    
+                </div>
             </header>
         </div>
         );
